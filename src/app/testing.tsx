de79c1482814f55f425c5ed97e9666cde6be9f85
Hieu Le Van <lehieu99666@gmail.com>
2024-05-13 13:32:21 +0700

    Add MUI Minimals into project

---
+++ src/app/testing.tsx
@@ -0,0 +1,31 @@
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+ 
+export default function Testing() {
+  return (
+    <Stack spacing={1} alignItems="center">
+      <Button variant="contained">Button</Button>
+      <Button variant="contained" color="primary">
+        Button
+      </Button>
+      <Button variant="contained" color="secondary">
+        Button
+      </Button>
+      <Button variant="contained" color="info">
+        Button
+      </Button>
+      <Button variant="contained" color="success">
+        Button
+      </Button>
+      <Button variant="contained" color="warning">
+        Button
+      </Button>
+      <Button variant="contained" color="error">
+        Button
+      </Button>
+      <Button disabled variant="contained">
+        Button
+      </Button>
+    </Stack>
+  );
+}
