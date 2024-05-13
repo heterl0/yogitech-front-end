0d997320955ff2e58f20303e60fa8bdcf55ed395
Hieu Le Van <lehieu99666@gmail.com>
2024-05-13 17:52:47 +0700

    add localization to project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ThemeProvider from "@/theme";
+import { LocalizationProvider } from "@/locales";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>{children}</ThemeProvider>
+        <LocalizationProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </LocalizationProvider>
       </body>
     </html>
   );
---
+++ src/locales/use-locales.ts
@@ -0,0 +1,44 @@
+"use client";
+
+import { useCallback } from "react";
+import { useTranslation } from "react-i18next";
+import { allLangs, defaultLang } from "./config-lang";
+import { localStorageGetItem } from "@/utils/storage-available";
+import { useSettingsContext } from "@/components/settings";
+
+// ----------------------------------------------------------------------
+
+export function useLocales() {
+  const langStorage = localStorageGetItem("i18nextLng");
+
+  const currentLang =
+    allLangs.find((lang) => lang.value === langStorage) || defaultLang;
+
+  return {
+    allLangs,
+    currentLang,
+  };
+}
+
+// ----------------------------------------------------------------------
+
+export function useTranslate() {
+  const { t, i18n, ready } = useTranslation();
+
+  const settings = useSettingsContext();
+
+  const onChangeLang = useCallback(
+    (newlang: string) => {
+      i18n.changeLanguage(newlang);
+      settings.onChangeDirectionByLang(newlang);
+    },
+    [i18n, settings]
+  );
+
+  return {
+    t,
+    i18n,
+    ready,
+    onChangeLang,
+  };
+}
