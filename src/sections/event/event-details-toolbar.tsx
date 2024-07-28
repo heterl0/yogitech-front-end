33aac3dc5409cc3191b786249aa12660c3f384fe
Hieu Le Van <lehieu99666@gmail.com>
2024-07-28 19:41:24 +0700

    refactor: Use strict equality (===) in color conditions for PostDetailsToolbar and UserTableRow components
    This commit updates the color conditions in the PostDetailsToolbar and UserTableRow components to use strict equality (===) instead of loose equality (==). By using strict equality, the color rendering based on the value of the option or active_status variables becomes more consistent and reliable. This change improves the accuracy and stability of the user interface in the blog and user sections.

--- src/sections/event/event-details-toolbar.tsx
+++ src/sections/event/event-details-toolbar.tsx
@@ -1,17 +1,12 @@
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
-import MenuItem from "@mui/material/MenuItem";
 import IconButton from "@mui/material/IconButton";
-import LoadingButton from "@mui/lab/LoadingButton";
 import Stack, { StackProps } from "@mui/material/Stack";
 
 import { RouterLink } from "@/routes/components";
 
 import Iconify from "@/components/iconify";
-import CustomPopover, { usePopover } from "@/components/custom-popover";
-import { LabelColor } from "@/components/label";
-import { EVENT_STATUS } from "@/types/event";
 import { useTranslation } from "react-i18next";
 
 // ----------------------------------------------------------------------
@@ -19,26 +14,15 @@ import { useTranslation } from "react-i18next";
 type Props = StackProps & {
   backLink: string;
   editLink: string;
-  status: number;
-  onChangeStatus: (newValue: number) => void;
-  statusOptions: {
-    value: number;
-    label: string;
-    color: LabelColor;
-  }[];
 };
 
 export default function EventDetailsToolbar({
   backLink,
   editLink,
-  onChangeStatus,
-  statusOptions,
-  status,
   sx,
   ...other
 }: Props) {
   const { t } = useTranslation();
-  const popover = usePopover();
 
   return (
     <>
@@ -66,43 +50,7 @@ export default function EventDetailsToolbar({
             <Iconify icon="solar:pen-bold" />
           </IconButton>
         </Tooltip>
-
-        <LoadingButton
-          color="inherit"
-          variant="contained"
-          // loading={!status}
-          loadingIndicator={t("eventPage.eventDetails.toolbar.loading")}
-          endIcon={<Iconify icon="eva:arrow-ios-downward-fill" />}
-          onClick={popover.onOpen}
-          sx={{ textTransform: "capitalize" }}
-        >
-          {EVENT_STATUS[status].label}
-        </LoadingButton>
       </Stack>
-
-      <CustomPopover
-        open={popover.open}
-        onClose={popover.onClose}
-        arrow="top-right"
-        sx={{ width: 140 }}
-      >
-        {statusOptions.map((option) => (
-          <MenuItem
-            key={option.value}
-            selected={option.value === status}
-            onClick={() => {
-              popover.onClose();
-              onChangeStatus(option.value);
-            }}
-          >
-            {option.value === 0 && <Iconify icon="eva:cloud-upload-fill" />}
-            {option.value === 1 && <Iconify icon="solar:file-text-bold" />}
-            {option.value === 2 && <Iconify icon="solar:file-text-bold" />}
-            {option.value === 3 && <Iconify icon="solar:file-text-bold" />}
-            {option.label}
-          </MenuItem>
-        ))}
-      </CustomPopover>
     </>
   );
 }
--- src/sections/event/view/event-details-view.tsx
+++ src/sections/event/view/event-details-view.tsx
@@ -1,15 +1,13 @@
 "use client";
 
-import { useState, useCallback, useEffect } from "react";
+import { useState, useCallback } from "react";
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import Container from "@mui/material/Container";
 import { paths } from "@/routes/paths";
 import { useSettingsContext } from "@/components/settings";
 import { useGetEvent } from "@/api/event";
-import { EVENT_STATUS } from "@/types/event";
 import EventDetailsContent from "../event-details-content";
-import axiosInstance, { endpoints } from "@/utils/axios";
 import Label from "@/components/label";
 import EventDetailsCandidates from "../event-details-candidates";
 import EventDetailsToolbar from "../event-details-toolbar";
@@ -26,8 +24,6 @@ export default function EventDetailsView({ id }: Props) {
 
   const { event: currentEvent } = useGetEvent(id);
 
-  const [status, setStatus] = useState(0);
-
   const [currentTab, setCurrentTab] = useState("detail");
 
   const { t } = useTranslation();
@@ -37,55 +33,11 @@ export default function EventDetailsView({ id }: Props) {
     { value: "ranking", label: t("eventPage.tabs.ranking") },
   ];
 
-  useEffect(() => {
-    if (currentEvent) {
-      if (currentEvent.active_status === 0) {
-        setStatus(0);
-      } else {
-        if (currentEvent.status === 0) {
-          setStatus(1);
-        } else if (currentEvent.status === 1) {
-          setStatus(2);
-        } else {
-          setStatus(3);
-        }
-      }
-    }
-  }, [currentEvent]);
-
   const handleChangeTab = useCallback(
     (event: React.SyntheticEvent, newValue: string) => {
       setCurrentTab(newValue);
     },
     []
-  );
-
-  const handleChangeStatus = useCallback(
-    (newValue: number) => {
-      let active_status = 0;
-      let status = 0;
-      if (newValue === 0) {
-        active_status = 0;
-        status = 0;
-      } else {
-        active_status = 1;
-        if (newValue === 1) {
-          status = 0;
-        } else if (newValue === 2) {
-          status = 1;
-        } else {
-          status = 2;
-        }
-      }
-      axiosInstance.patch(`${endpoints.event.update(currentEvent.id + "")}`, {
-        active_status,
-        status,
-        exercise_ids: currentEvent.exercises.map((exercise) => exercise.id),
-      });
-
-      setStatus(newValue);
-    },
-    [currentEvent]
   );
 
   const renderTabs = (
@@ -121,9 +73,6 @@ export default function EventDetailsView({ id }: Props) {
       <EventDetailsToolbar
         backLink={paths.dashboard.event.root}
         editLink={paths.dashboard.event.edit(`${currentEvent?.id}`)}
-        status={status || 0}
-        onChangeStatus={handleChangeStatus}
-        statusOptions={EVENT_STATUS}
       />
       {renderTabs}
 
