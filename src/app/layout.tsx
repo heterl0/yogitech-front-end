b9447f278b121217598f5bca42e8af42ff69af9d
Hieu Le Van <lehieu99666@gmail.com>
2025-03-12 23:43:54 +0700

    feat: Update slick-carousel version and add styles; enhance image rendering in carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,16 @@ export default function RootLayout({
             </Script>
           </>
         )}
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
       </body>
     </html>
   );
