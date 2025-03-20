175c25b11a8b3aa994c7ffaa49608188ced010be
Hieu Le Van <lehieu99666@gmail.com>
2025-03-21 01:11:35 +0700

    feat: Refactor MobileBanner layout for improved responsiveness and styling

--- src/components/sidebar/mobile-banner.tsx
+++ src/components/sidebar/mobile-banner.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
@@ -29,7 +28,7 @@ export default function MobileBanner() {
         display: { xs: "block", md: "none" },
       }}
     >
-      <Stack direction="row" spacing={2} alignItems="center">
+      <div className="flex flex-row items-center justify-between gap-2">
         <IconButton
           size="small"
           onClick={() => setIsOpen(false)}
@@ -38,23 +37,30 @@ export default function MobileBanner() {
           <Iconify icon="eva:close-fill" width={20} />
         </IconButton>
 
-        <Box
-          component="img"
-          src="/logo/logo_signle_2.svg?height=60&width=60"
-          alt="App Icon"
-          sx={{ width: 60, height: 60, borderRadius: 1 }}
-        />
+        <div className="flex items-center gap-2">
+          <Box
+            component="img"
+            src="/logo/logo_signle_2.svg?height=60&width=60"
+            alt="App Icon"
+            sx={{ width: 60, height: 60, borderRadius: 1 }}
+          />
 
-        <Stack spacing={0.5}>
-          <Typography variant="subtitle2" noWrap>
-            {t("mobileBanner.downloadApp")}
-          </Typography>
-          <Typography variant="caption" sx={{ color: "text.secondary" }} noWrap>
-            {t("mobileBanner.description")}
-          </Typography>
-        </Stack>
+          <div className="flex w-full max-w-40 flex-col gap-1">
+            <Typography variant="subtitle2" noWrap>
+              {t("mobileBanner.downloadApp")}
+            </Typography>
+            <Typography
+              variant="caption"
+              sx={{ color: "text.secondary" }}
+              noWrap
+            >
+              {t("mobileBanner.description")}
+            </Typography>
+          </div>
+        </div>
 
         <Button
+          className="flex-none"
           variant="contained"
           size="small"
           href="https://storage.yogitech.me/files/yogitech-beta-v1_0.1.apk"
@@ -62,7 +68,7 @@ export default function MobileBanner() {
         >
           {t("mobileBanner.getButton")}
         </Button>
-      </Stack>
+      </div>
     </Box>
   );
 }
